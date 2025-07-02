cb8e40c5cc376cc14451eaeec0eb9ae297d35124
Lyria-chan <maja.knaflewska@gmail.com>
2025-07-02 17:15:39 +0200

    Got rid of redundant/small files, moved them elsewhere

--- kubejs_scripts/server_scripts/lava_chicken.js
+++
@@ -1,8 +0,0 @@
-/*LootJS.modifiers((event) => {
-    event
-        .addEntityLootModifier("minecraft:chicken")
-        .matchDamageSource((source) => {
-            // Your code. Check DamageSourcePredicateBuilderJS for more information
-        }) 
-        .addLoot("minecraft:diamond");
-});*/
--- kubejs_scripts/startup_scripts/edible_tapioca.js
+++
@@ -1,9 +0,0 @@
-ItemEvents.modification(event => {
-  event.modify('cozyfoods:tapioca_pearls', item => {
-    item.foodProperties = food => {
-        food.hunger(1)
-        food.saturation(1)
-    }
-  })
-
-})
